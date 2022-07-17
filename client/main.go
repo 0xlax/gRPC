654ee6f1f994476a90563fd74380cf1f13a1c0d3
0xlax <harshu8310@gmail.com>
2022-07-17 16:28:09 +0530

    client

--- client/main.go
+++ client/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "flag"
+import (
+	"context"
+	"flag"
+
+	"github.com/0xlax/gRPC"
+)
+
+
+var client proto.BLockchainClient
 
 func main() {
 	addFlag := flag.Bool("add", false, "add new block")
@@ -12,6 +20,34 @@ func main() {
 		log.Fataf("canot dial server: %v", err)
 	}
 
-	client := proto
+	client := proto.NewBLockchainClient(conn)
 
+	if *addFlag {
+		AddBlock()
+	}
+
+	iff *listflag{
+		getBlockchain()
+	}
+
+}
+
+
+func addBlock() {
+	block, err := client.AddBlock(context.Background(), &proto.AddBlockRequest{
+		Data: time.Now().ToString()
+	})
+	if err != nill {
+		log.Fatalf("unable to add block")
+	}
+	log.Printf("new block hash: %s\n", block.Hash)
+}
+
+func getBlockchain() {
+	bc, err := client.GetBlockchain(context.Background(), &proto.GetBlockchainRequest{})
+	if err != nill {
+		log.Fatalf("unable to get")
+	}
+	log.Printf("blocks")
+	// Print WIP- Print Current.block contents
 }
